6e48abd1541cbeb14b1f94fc4780e6ffab34cb1c
KamiGamesHost <63718604+KamiGamesHost@users.noreply.github.com>
2020-09-03 17:00:22 +0200

    Working on Player Controller

---
+++ The Legend of Armstrong/Assets/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Initialize the private variables
+    private Camera cameraComp = null;
+
+    public Camera CameraComp
+    {
+        get { return cameraComp; }
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    // Initialize the camera controller
+    private void Initialize()
+    {
+        cameraComp = GetComponent<Camera>();
+    }
+}
---
+++ The Legend of Armstrong/Assets/Cursor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        RaycastHit hit = new RaycastHit();
+        Ray ray = GameManager.Instance._Camera.CameraComp.ScreenPointToRay(Input.mousePosition);
+        LayerMask mask = LayerMask.GetMask("CursorCollider");
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, mask))
+        {
+            transform.position = hit.point;
+        }
+    }
+}
