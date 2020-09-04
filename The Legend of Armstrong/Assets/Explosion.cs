5f01c50e515fd4a3e866a3bf87f9995f8789b506
KamiGamesHost <63718604+KamiGamesHost@users.noreply.github.com>
2020-09-04 14:33:40 +0200

    Somewhat polished build I guess

---
+++ The Legend of Armstrong/Assets/Explosion.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float force = 500f;
+
+    private float destroyTimer = 1f;
+    private int damage = 0;
+
+    public int Damage
+    {
+        set { damage = value; }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (destroyTimer <= 0f)
+        {
+            Destroy(gameObject);
+        }
+
+        destroyTimer--;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        IDamageable<int> damageable = other.GetComponentInParent<IDamageable<int>>();
+        PlayerController player = other.GetComponentInParent<PlayerController>();
+        if (damageable != null)
+        {
+            if (player == null)
+            {
+                damageable.TakeDamage(damage);
+                Destroy(gameObject);
+            }
+            else
+            {
+                Vector3 direction = (player.transform.position - transform.position).normalized;
+                player.Blast(direction, force);
+            }
+        }
+    }
+}
---
+++ The Legend of Armstrong/Assets/Projectile.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // Initialize the private variables
+    private Rigidbody rb = null;
+    private float projectileSpeed = 0f;
+    private int damage = 1;
+    public bool isHostile = false;
+
+    public float ProjectileSpeed
+    {
+        set { projectileSpeed = value; }
+    }
+
+    public int Damage
+    {
+        set { damage = value; }
+    }
+
+    public bool IsHostile
+    {
+        set { isHostile = value; }
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IDamageable<int> damageable = other.GetComponentInParent<IDamageable<int>>();
+        PlayerController player = other.GetComponentInParent<PlayerController>();
+        if (damageable != null)
+        {
+            if ((isHostile && player != null) || (!isHostile && player == null))
+            {
+                damageable.TakeDamage(damage);
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Initialize the projectile
+    private void Initialize()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.right * projectileSpeed;
+    }
+}
